b5ffb31e663856f6bac55fe7fb3752c52c706b1b
hp <755991743@qq.com>
2026-06-12 16:38:03 +0800

    fix: add -parameters compiler flag, hide passwordHash via @JsonIgnore

--- backend/virtual-lover-dao/src/main/java/com/virtuallover/dao/entity/User.java
+++ backend/virtual-lover-dao/src/main/java/com/virtuallover/dao/entity/User.java
@@ -1,6 +1,7 @@
 package com.virtuallover.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import java.time.LocalDateTime;
 
@@ -10,6 +11,7 @@ public class User {
     @TableId(type = IdType.AUTO)
     private Long id;
     private String username;
+    @JsonIgnore
     private String passwordHash;
     private String nickname;
     private String avatarUrl;
